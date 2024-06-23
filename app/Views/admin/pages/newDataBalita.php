1324f37f1bc804d5e729c5e7e8f53958d6233fe3
Shofi Setiawan <shofistwn@gmail.com>
2024-06-23 10:27:45 +0700

    add images

--- app/Views/admin/pages/newDataBalita.php
+++ app/Views/admin/pages/newDataBalita.php
@@ -89,15 +89,14 @@
         <div class="modal-dialog modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="modalLakiLabel">Modal L</h1>
+                    <h1 class="modal-title fs-5" id="modalLakiLabel">IMT Anak Laki-Laki</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <img src="<?= base_url('imt-laki.png') ?>" alt="" class="w-full img-fluid">
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
                 </div>
             </div>
         </div>
@@ -107,15 +106,14 @@
         <div class="modal-dialog modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="modalPerempuanLabel">Modal P</h1>
+                    <h1 class="modal-title fs-5" id="modalPerempuanLabel">IMT Anak Perempuan</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    <img src="<?= base_url('imt-perempuan.png') ?>" alt="" class="w-full img-fluid">
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
                 </div>
             </div>
         </div>
